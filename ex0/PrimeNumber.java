a768793af09cd3ccb01837572d426a42d6ba2d0b
Vinothgopi graj <vinothgopi97@gmail.com>
2017-10-31 12:25:02 +0530

    added ex0

---
+++ ex0/PrimeNumber.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class Prime
+{
+	public static void main(String[] arg)
+	{
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter a number : ");
+		int number=scan.nextInt();
+		boolean flag=true;
+		for(int i=2;i<=number/2;i++)
+		{
+			if(number%i==0)
+			{
+				flag=false;
+				break;
+			}
+		}
+		if(flag==false)
+			System.out.println("The number "+number+" is not a Prime number");
+		else
+			System.out.println("The number "+number+" is a Prime number");
+	}
+}
+class CheckPrime
+{
+	int no;
+}
